975a650902c0d51ed07f3e23b1b692af95ae7aad
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-27 15:30:58 +0300

    Adding Validation step 1

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
+
   render() {
     return (
-      <div>
-        Post New
-      </div>
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
 
-export default PostsNew;
+  return erros;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
