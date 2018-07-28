2785be67d9f97b14b27715ce54cdf4350a71927f
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-28 13:01:36 +0300

    Confirure Deleting Posts

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 
 class PostsShow extends Component {
+
+  onDeleteClick() {
+    const { id } = this.props.match.params
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
   componentDidMount() {
-    const { id } = this.props.match.params.id;
+    const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
 
@@ -18,10 +27,16 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back to link</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
-      </div>
+      </div >
     );
   }
 }
@@ -30,4 +45,4 @@ function mapSateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapSateToProps, { fetchPost })(PostsShow);
+export default connect(mapSateToProps, { fetchPost, deletePost })(PostsShow);
