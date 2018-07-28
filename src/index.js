ec58ae206ba651e5591ccb24851b8b8ec0993e06
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-28 12:11:30 +0300

    Adding root parameters

--- src/index.js
+++ src/index.js
@@ -9,19 +9,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import reducers from './reducers';
 import PostIndex from './components/post_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/posts/new" component={PostsNew} />
-          <Route path="/" component={PostIndex} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/posts/:id" component={PostsShow} />
+                    <Route path="/" component={PostIndex} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
