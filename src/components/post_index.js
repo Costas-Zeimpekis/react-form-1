1c2c3305d3679f4abf240829eec06a7ea8424aaa
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-07 12:16:14 +0300

    Refactorin part 1

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -13,29 +13,26 @@ class PostIndex extends Component {
     return _.map(this.props.posts, post => {
       return (
         <li key={post.id} className="list-group-item">
-          <Link to={`/posts/${post.id}`}>
-            {post.title}
-          </Link>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
         </li>
-      )
+      );
     });
   }
+
   render() {
     return (
       <div>
         <div className="text-xs-right">
           <Link className="btn btn-primary" to="/posts/new">
             Add a Post
-        </Link>
+          </Link>
         </div>
         <div>
           <h3>Posts</h3>
-          <ul className="list-group">
-            {this.renderPosts()}
-          </ul>
+          <ul className="list-group">{this.renderPosts()}</ul>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -43,5 +40,7 @@ function mapStateToProps(state) {
   return { posts: state.posts };
 }
 
-
-export default connect(mapStateToProps, { fetchPosts })(PostIndex);
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostIndex);
